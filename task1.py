bd1d6ba43ccfe8750e9fa74d680fa6d1911b00a6
johnD-git005 <johndakshak@gmail.com>
2025-08-30 21:17:03 +0100

    Initial commit

---
+++ task1.py
@@ -0,0 +1,17 @@
+"""# 1
+def task1_sum_of_two_numbers(a, b):
+    Task 1:
+    Write a *function that accepts two numbers (a and b) as parameters
+    and returns their sum.
+    Test the function by calling it with different numbers.
+  
+    pass """
+
+def task1_sum_of_two_numbers(a, b):
+	return (a + b)
+
+print(task1_sum_of_two_numbers(2.5, 5))
+print(task1_sum_of_two_numbers(7, 3))
+print(task1_sum_of_two_numbers(8, 4.4))
+print(task1_sum_of_two_numbers(9, 1))
+print(task1_sum_of_two_numbers(6, 0))
