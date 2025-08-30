9543b3c57e4989f89946961f6b298e6905caffbc
johnD-git005 <johndakshak@gmail.com>
2025-08-30 21:52:40 +0100

    Initial commit

---
+++ task6.py
@@ -0,0 +1,16 @@
+"""# 6
+def task6_celsius_to_fahrenheit(celsius):
+    
+    Task 6:
+    Write a function that converts a temperature from Celsius to Fahrenheit.
+    Formula: (celsius * 9/5) + 32
+    
+    pass"""
+
+def task6_celsius_to_fahrenheit(celsius):
+	temperature = (celsius * 9/5) + 32
+	return temperature
+
+print(task6_celsius_to_fahrenheit(5))
+print(task6_celsius_to_fahrenheit(15))
+print(task6_celsius_to_fahrenheit(6))
