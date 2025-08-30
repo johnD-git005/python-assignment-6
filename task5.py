4fadca26f1f28451f14223182a0cc51d7258fadc
johnD-git005 <johndakshak@gmail.com>
2025-08-30 21:42:09 +0100

    Initial commit

---
+++ task5.py
@@ -0,0 +1,19 @@
+"""# 5
+def task5_perimeter_of_square(side):
+    
+    Task 5:
+    Write a function that accepts the side length of a square
+    and returns its perimeter.
+    Formula: perimeter = 4 * side
+    
+    pass"""
+
+def task5_perimeter_of_square(side):
+	perimeter = 4 * side
+	return perimeter
+
+print(task5_perimeter_of_square(5))
+print(task5_perimeter_of_square(7))
+print(task5_perimeter_of_square(3))
+print(task5_perimeter_of_square(8))
+print(task5_perimeter_of_square(9))
