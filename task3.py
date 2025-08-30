bd631f4c1a387015d4fb207f9555995036907b5d
johnD-git005 <johndakshak@gmail.com>
2025-08-30 21:28:22 +0100

    Initial commit

---
+++ task3.py
@@ -0,0 +1,17 @@
+"""# 3
+def task3_greet_user(name):
+    Task 3:
+    Write a function that accepts a person's name as a parameter
+    and prints a greeting message like: "Hello, John!"
+   
+    pass"""
+
+def task3_greet_user(name):
+	return f"Hello {name}!"
+
+print(task3_greet_user("John"))
+print(task3_greet_user("Tom"))
+print(task3_greet_user("James"))
+print(task3_greet_user("Ruth"))
+print(task3_greet_user("David"))
+
