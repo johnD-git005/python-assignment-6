be73a4b92fd294f34fff1136d38863c7dfd5013e
johnD-git005 <johndakshak@gmail.com>
2025-08-30 21:23:59 +0100

    Initial commit

---
+++ task2.py
@@ -0,0 +1,17 @@
+"""# 2
+def task2_square_number(n):
+    
+    Task 2:
+    Write a function that accepts a number and returns its square.
+    Example: square_number(5) → 25
+    
+    pass """
+
+def task2_square_number(n):
+	return (n * n)
+
+print(task2_square_number(5))
+print(task2_square_number(6))
+print(task2_square_number(4))
+print(task2_square_number(3))
+print(task2_square_number(2))
