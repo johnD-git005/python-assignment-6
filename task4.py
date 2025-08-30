56f2fe8ec1c50992c16fc8ff67e0f06aaff1a99c
johnD-git005 <johndakshak@gmail.com>
2025-08-30 21:36:20 +0100

    initial commit

---
+++ task4.py
@@ -0,0 +1,17 @@
+"""# 4
+def task4_area_of_rectangle(length, width):
+    
+    Task 4:
+    Write a function that accepts the length and width of a rectangle
+    and returns its area.
+    Formula: area = length * width
+    
+    pass"""
+
+def task4_area_of_rectangle(length, width):
+	area = length * width
+	return area
+
+print(task4_area_of_rectangle(2.5, 5))
+print(task4_area_of_rectangle(3, 5))
+print(task4_area_of_rectangle(6, 7.5))
